dd344c6767e325334f4251d95bf0c1fe979bede4
Akinlowo Olumide <adeolaolumide9@gmail.com>
2023-07-31 20:48:34 +0000

    Corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,49 @@
-te Flask app that works with the API
-"""
+#!/usr/bin/python3
+""" Flask Application """
 from models import storage
-from flask import Flask, jsonify
-from flask_cors import CORS
 from api.v1.views import app_views
-import os
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """
-    tears down
-    """
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
     """
-    error handler for 404
-    """
-    return (jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,30 @@
-tes a route /status on any object app_views
-return: JSON: "status: OK"
-"""
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
+#!/usr/bin/python3
+""" Index """
 from models.amenity import Amenity
-from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-classes = {"amenities": Amenity, "cities": City, "places": Place,
-           "reviews": Review, "states": State, "users": User}
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """
-    Returns OK if JSON works
-    """
-    j_status = {"status": "OK"}
-    return jsonify(j_status)
+    """ Status of API """
+    return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def count_all():
-    """
-    Returns OK if JSON works
-    """
-    return jsonify({
-        name: storage.count(obj) for name, obj in classes.items()
-    })
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
