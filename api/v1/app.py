8bbecf5d3b5b8b4d2e0b29550af2a9965f438ad0
Akinlowo Olumide <adeolaolumide9@gmail.com>
2023-08-01 05:25:31 +0000

    Updated my Modules so I can call storage

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Flask Application """
+
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -7,6 +8,9 @@ from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
+
+storage.reload()
+
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
